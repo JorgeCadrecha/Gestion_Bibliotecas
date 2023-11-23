d349af32edeccc330fba983e9044609b41481e79
Jorge <jcadrdel@myuax.es>
2023-11-23 18:43:02 +0100

    Initial commit

---
+++ src/main/java/com/example/Gestion_Bibliotecas/GestionBibliotecasApplication.java
@@ -0,0 +1,13 @@
+package com.example.Gestion_Bibliotecas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionBibliotecasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionBibliotecasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Gestion_Bibliotecas/GestionBibliotecasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Gestion_Bibliotecas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionBibliotecasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
